9c5af7b95fd214129adc61b211f1d1bcb271abdc
santaes <oleksandrsukharyev@gmail.com>
2022-03-30 13:44:04 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+const menuButton = document.querySelector('#menu');
+const navMenu = document.querySelector('nav');
+
+menuButton.addEventListener('click', function () {
+  if (navMenu.classList.contains('d-none')) {
+    navMenu.classList.add('d-flex');
+    navMenu.classList.remove('d-none');
+  } else {
+    navMenu.classList.remove('d-flex');
+    navMenu.classList.add('d-none');
+  }
+});
